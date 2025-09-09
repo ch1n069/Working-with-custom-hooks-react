0d46bff59c94eeaec4f470359284de6bdf577417
Bruno Alpachino <brunolalpachino@gmail.com>
2025-09-09 11:42:11 +0300

    Init first commit

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ In this project we are going to be doing a deep dive into working with hooks in 
 ```bash
 
 ```
+
+# UseOnline
+
+- We are going to start with this hook which will help us detect when the user is online.
+- We can then later in any section of the app when the user is online or offline show various components depending, on their online status.
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useOnline } from "./hooks/useOnline";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const isOnline = useOnline();
   return (
     <>
       <div>
-        <span className="text-red">hi there</span>
+        {/* <span className="text-3xl text-red-400">hi there</span> */}
+        <p>{isOnline ? "✅ Online" : "❌ Offline"}</p>
       </div>
     </>
   );
